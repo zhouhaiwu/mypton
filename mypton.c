e0f1b4e91dc944f79e6d872981d160a02be2386c
zhou199611 <zhou199019901@163.com>
2021-03-11 20:56:51 +0800

    第一次提交

---
+++ mypton.c
@@ -0,0 +1,26 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    struct in_addr inaddr;
+    int rc;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <dotted-decimal>\n", argv[0]);
+        exit(0);
+    }
+
+    rc = inet_pton(AF_INET, argv[1], &inaddr);
+    if (rc == 0)
+        printf("inet_pton error: invalid dotted-decimal address");
+        //app_error("inet_pton error: invalid dotted-decimal address");
+    else if (rc < 0)
+        //unix_error("inet_pton error");
+        printf("inet_pton error\n");
+
+    printf("0x%x\n", ntohl(inaddr.s_addr));
+    exit(0);
+
+}
